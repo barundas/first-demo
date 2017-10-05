adcb7c8c10f843f73b3067cfc96ebfbfff8e44f7
Administrator <Administrator@win2012base>
2017-10-05 07:55:00 -0700

    init

---
+++ src/main/java/com/cts/fsd/firstdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cts.fsd.firstdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstDemoApplication.class);
+	}
+
+}
---
+++ src/main/java/com/cts/fsd/firstdemo/controllers/main/WelcomeController.java
@@ -0,0 +1,34 @@
+package com.cts.fsd.firstdemo.controllers.main;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
+	@Value("${spring.application.name}")
+	private String APP_NAME;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String welcome() {
+		String msg = "Welcome to the new " + APP_NAME + " World 2017.";
+		logger.info(msg);
+		return msg;
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String home() {
+		String msg = "Welcome to the new " + APP_NAME + " World 2017 Home.";
+		logger.info(msg);
+		return msg;
+	}
+}
